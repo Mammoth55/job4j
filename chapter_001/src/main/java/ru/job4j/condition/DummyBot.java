681e3f5749a852972488b94657bc341678d4321e
Mammoth55 <slamam@mail.ru>
2020-02-20 00:49:06 +0600

    4.1. Глупый бот.[#233914]

---
+++ chapter_001/src/main/java/ru/job4j/condition/DummyBot.java
@@ -0,0 +1,22 @@
+package ru.job4j.condition;
+
+public class DummyBot {
+
+    public static String answer(String question) {
+        String rsl = "Это ставит меня в тупик. Задайте другой вопрос.";
+        if ("Привет, Бот.".equals(question)) {
+            // заменить ... на правильный ответ rsl = "ответ по заданию".
+            rsl = "Привет, Умник.";
+        } else if ("Пока.".equals(question)) {
+            // заменить ... на правильный ответ rsl = "ответ по заданию".
+            rsl = "До скорой встречи.";
+        }
+        return rsl;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
+
+
