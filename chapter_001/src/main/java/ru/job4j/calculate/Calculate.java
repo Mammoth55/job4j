ed45e15c33d8e8c61e13c7ae0d6fcfeac411a45c
Mammoth55 <slamam@mail.ru>
2020-02-25 00:32:07 +0600

    5.8. JavaDoc. Code convention[#234004]

---
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -0,0 +1,21 @@
+package ru.job4j.calculate;
+
+/**
+ * Calculate task.
+ * @author Viacheslav Mamontov (slamam@mail.ru)
+ * @version 1.0a
+ * @since 24.02.2020
+ */
+public class Calculate {
+
+    /**
+     * Main.
+     * @param args - args.
+     * @author Viacheslav Mamontov (slamam@mail.ru)
+     * @since 24.02.2020
+     */
+    public static void main(String[] args) {
+
+    }
+
+}
---
+++ chapter_001/src/main/java/ru/job4j/calculate/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Package for calculate task.
+ *
+ * @author Viacheslav Mamontov (slamam@mail.ru)
+ * @version 1.0a
+ * @since 24.02.2020
+ */
+package ru.job4j.calculate;
