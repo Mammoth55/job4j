a4ff284f029d2c6abaee2af6c0d7b4a51c926ac6
Mammoth55 <slamam@mail.ru>
2020-02-26 19:58:15 +0600

    6.5. Поиск индекса в диапазоне.[#233932]

--- chapter_001/src/test/java/ru/job4j/array/FindLoopTest.java
+++ chapter_001/src/test/java/ru/job4j/array/FindLoopTest.java
@@ -9,16 +9,14 @@ public class FindLoopTest {
     @Test
     public void whenArrayHas5Then0() {
         FindLoop find = new FindLoop();
-        int[] input = new int[] {5, 10, 3};
-        // int value = 5;
+        int[] input = new int[] {0, 10, 3, 5, 11};
         int result = find.indexOf(input, 5);
-        // int expect = 0;
-        assertThat(result, is(0));
+        assertThat(result, is(3));
     }
     @Test
     public void whenArrayHas1ThenMinus1() {
         FindLoop find = new FindLoop();
-        int[] input = new int[] {5, 10, 3};
+        int[] input = new int[] {45, 10, 33, 20, 11};
         // int value = 5;
         int result = find.indexOf(input, 1);
         // int expect = 0;
