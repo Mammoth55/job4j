cd7d7c5766ae71c48582c8a174108dec3b7067b8
Mammoth55 <slamam@mail.ru>
2020-03-03 21:00:49 +0600

    3. Перегрузить метод max для трех чисел[#234092]

--- chapter_001/src/main/java/ru/job4j/condition/Max.java
+++ chapter_001/src/main/java/ru/job4j/condition/Max.java
@@ -1,9 +1,19 @@
 package ru.job4j.condition;
 
 public class Max {
-    public static int max(int left, int right) {
-        boolean more = left > right;
-        int result = more ? left : right;
+
+    public static int max(int first, int second) {
+        int result = first > second ? first : second;
+        return result;
+    }
+
+    public static int max(int first, int second, int third) {
+        int result = first > max(second, third) ? first : max(second, third);
+        return result;
+    }
+
+    public static int max(int first, int second, int third, int fourth) {
+        int result = max(first, fourth) > max(second, third) ? max(first, fourth) : max(second, third);
         return result;
     }
 }
--- chapter_001/src/test/java/ru/job4j/condition/MaxTest.java
+++ chapter_001/src/test/java/ru/job4j/condition/MaxTest.java
@@ -5,19 +5,28 @@ import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
 public class MaxTest {
+
     @Test
-    public void whenMax1To2Then2() {
+    public void whenMax1() {
+        int result = Max.max(11, -2, 0, 9);
+        assertThat(result, is(11));
+    }
+
+    @Test
+    public void whenMax2() {
         int result = Max.max(1, 2);
         assertThat(result, is(2));
     }
+
     @Test
-    public void whenMax2To1Then1() {
-        int result = Max.max(3, 1);
-        assertThat(result, is(3));
+    public void whenMax3() {
+        int result = Max.max(3, 1, 7);
+        assertThat(result, is(7));
     }
+
     @Test
-    public void whenEqual1and2() {
-        int result = Max.max(5, 5);
+    public void when4Equal() {
+        int result = Max.max(5, 5, 5, 5);
         assertThat(result, is(5));
     }
 }
