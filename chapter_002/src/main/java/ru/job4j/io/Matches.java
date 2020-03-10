7ad02c7dcf730927bc132212eac41d60913e3c09
Mammoth55 <slamam@mail.ru>
2020-03-11 01:20:59 +0600

    2. Scanner и чтение числа из консоли.[#234062] рефакторинг

--- chapter_002/src/main/java/ru/job4j/io/Matches.java
+++ chapter_002/src/main/java/ru/job4j/io/Matches.java
@@ -4,30 +4,34 @@ import java.util.Scanner;
 
 public class Matches {
 
-    public static void main(String[] args) {
-        System.out.println("Игра 11");
-        String winner = "второй";
-        int matches = 11;
+    private int player;
+    private int number;
+
+    public Matches(int player, int number) {
+        this.player = player;
+        this.number = number;
+    }
+
+    public int validInput() {
         int select;
         Scanner input = new Scanner(System.in);
         do {
-            do {
-                System.out.println("На столе " + matches + " спичек.");
-                System.out.print("Ходит первый игрок. Сколько спичек убрать (1-3) ? ");
-                select = Integer.valueOf(input.nextLine());
-            } while (select < 1 || select > 3);
-            matches -= select;
-            if (matches < 1) {
-                winner = "первый";
-                break;
-            }
-            do {
-                System.out.println("На столе " + matches + " спичек.");
-                System.out.print("Ходит второй игрок. Сколько спичек убрать (1-3) ? ");
-                select = Integer.valueOf(input.nextLine());
-            } while (select < 1 || select > 3);
-            matches -= select;
-        } while (matches > 0);
-        System.out.println("Конец игры. Выиграл " + winner + " игрок, поздравляю !");
+            System.out.print("Ходит " + this.player + " игрок. Сколько спичек убрать (1-3) ? ");
+            select = Integer.valueOf(input.nextLine());
+        } while (select < 1 || select > 3);
+        return select;
+    }
+
+    public static void main(String[] args) {
+        Matches matches = new Matches(1, 11);
+        System.out.println("Игра 11");
+        int select;
+        while (matches.number > 0) {
+            System.out.println("На столе " + matches.number + " спичек.");
+            select = matches.validInput();
+            matches.number -= select;
+            matches.player = (matches.player % 2) + 1;
+        }
+        System.out.println("Конец игры. Выиграл " + (matches.player % 2 + 1) + " игрок, поздравляю !");
     }
 }
