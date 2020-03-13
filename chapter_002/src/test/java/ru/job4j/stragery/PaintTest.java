dd996d7e2be195c631f6cbb39db2558f4a94b0f4
Mammoth55 <slamam@mail.ru>
2020-03-13 23:56:34 +0600

    9.1 Рефакторинг теста @Before @After[#234059] приквел

--- chapter_002/src/test/java/ru/job4j/stragery/PaintTest.java
+++ chapter_002/src/test/java/ru/job4j/stragery/PaintTest.java
@@ -41,4 +41,32 @@ public class PaintTest {
         // возвращаем обратно стандартный вывод в консоль.
         System.setOut(stdout);
     }
+
+    @Test
+    public void whenDrawTriangle() {
+        // получаем ссылку на стандартный вывод в консоль.
+        PrintStream stdout = System.out;
+        // Создаем буфур для хранения вывода.
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        //Заменяем стандартный вывод на вывод в пямять для тестирования.
+        System.setOut(new PrintStream(out));
+        // выполняем действия пишушиее в консоль.
+        new Paint().draw(new Triangle());
+        // проверяем результат вычисления
+        assertThat(
+                new String(out.toByteArray()),
+                is(
+                        new StringJoiner(System.lineSeparator())
+                                .add("[]        ")
+                                .add("[][]      ")
+                                .add("[]  []    ")
+                                .add("[]    []  ")
+                                .add("[][][][][]")
+                                .add("")
+                                .toString()
+                )
+        );
+        // возвращаем обратно стандартный вывод в консоль.
+        System.setOut(stdout);
+    }
 }
