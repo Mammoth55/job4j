ec7e398f9cbc14bacefca0668ee4f470c29aef7f
Mammoth55 <slamam@mail.ru>
2020-04-01 18:31:52 +0600

    0. Set, HashSet, Iterator.[#234043]

--- chapter_002/src/test/java/ru/job4j/tracker/StartUITest.java
+++ chapter_002/src/test/java/ru/job4j/tracker/StartUITest.java
@@ -3,6 +3,7 @@ package ru.job4j.tracker;
 import org.junit.Test;
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
+import java.util.ArrayList;
 import java.util.StringJoiner;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
@@ -15,7 +16,7 @@ public class StartUITest {
                 new String[] {"0"}
         );
         StubAction action = new StubAction();
-        new StartUI().init(input, new Tracker(), new UserAction[] {action});
+        new StartUI().init(input, new Tracker(), new ArrayList<UserAction>());
         assertThat(action.isCall(), is(true));
     }
 
@@ -28,7 +29,7 @@ public class StartUITest {
                 new String[] {"0"}
         );
         StubAction action = new StubAction();
-        new StartUI().init(input, new Tracker(), new UserAction[] {action});
+        new StartUI().init(input, new Tracker(), new ArrayList<UserAction>());
         String expect = new StringJoiner(System.lineSeparator(), "", System.lineSeparator())
                 .add("Menu.")
                 .add("0. Stub action")
--- chapter_002/src/test/java/ru/job4j/tracker/TrackerTest.java
+++ chapter_002/src/test/java/ru/job4j/tracker/TrackerTest.java
@@ -1,6 +1,9 @@
 package ru.job4j.tracker;
 
 import org.junit.Test;
+
+import java.util.ArrayList;
+
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
 import static org.hamcrest.CoreMatchers.*;
@@ -26,49 +29,63 @@ public class TrackerTest {
     @Test
     public void whenAddNewItemsThenFindAll() {
         Tracker tracker = new Tracker();
-        Item[] item = new Item[4];
-        item[0] = new Item("test1");
-        tracker.add(item[0]);
-        item[1] = new Item("test2");
-        tracker.add(item[1]);
-        item[2] = new Item("test3");
-        tracker.add(item[2]);
-        item[3] = new Item("test4");
-        tracker.add(item[3]);
-        Item[] result = tracker.findAll();
-        assertThat(result, is(item));
+        ArrayList<Item> items = new ArrayList<Item>();
+        Item item = new Item("test1");
+        items.add(item);
+        tracker.add(item);
+        item = new Item("test2");
+        items.add(item);
+        tracker.add(item);
+        item = new Item("test3");
+        items.add(item);
+        tracker.add(item);
+        item = new Item("test4");
+        items.add(item);
+        tracker.add(item);
+        ArrayList<Item> result;
+        result = tracker.findAll();
+        assertThat(result, is(items));
     }
 
     @Test
     public void whenAddNewItemsThenFindById() {
         Tracker tracker = new Tracker();
-        Item[] item = new Item[4];
-        item[0] = new Item("test1");
-        tracker.add(item[0]);
-        item[1] = new Item("test2");
-        tracker.add(item[1]);
-        item[2] = new Item("test3");
-        tracker.add(item[2]);
-        item[3] = new Item("test4");
-        tracker.add(item[3]);
-        Item result = tracker.findById(item[1].getId());
-        assertThat(result, is(item[1]));
+        ArrayList<Item> items = new ArrayList<Item>();
+        Item item = new Item("test1");
+        items.add(item);
+        tracker.add(item);
+        item = new Item("test2");
+        items.add(item);
+        tracker.add(item);
+        item = new Item("test3");
+        items.add(item);
+        tracker.add(item);
+        item = new Item("test4");
+        items.add(item);
+        tracker.add(item);
+        Item result = tracker.findById(items.get(1).getId());
+        assertThat(result, is(items.get(1)));
     }
 
     @Test
     public void whenAddNewItemsThenFindByName() {
         Tracker tracker = new Tracker();
-        Item[] item = new Item[4];
-        item[0] = new Item("test1");
-        tracker.add(item[0]);
-        item[1] = new Item("test2");
-        tracker.add(item[1]);
-        item[2] = new Item("test3");
-        tracker.add(item[2]);
-        item[3] = new Item("test4");
-        tracker.add(item[3]);
-        Item[] result = tracker.findByName(item[2].getName());
-        assertThat(result[0].getName(), is(item[2].getName()));
+        ArrayList<Item> items = new ArrayList<Item>();
+        Item item = new Item("test1");
+        items.add(item);
+        tracker.add(item);
+        item = new Item("test2");
+        items.add(item);
+        tracker.add(item);
+        item = new Item("test3");
+        items.add(item);
+        tracker.add(item);
+        item = new Item("test4");
+        items.add(item);
+        tracker.add(item);
+        ArrayList<Item> result;
+        result = tracker.findByName(items.get(2).getName());
+        assertThat(result.get(0).getName(), is(items.get(2).getName()));
     }
 
     @Test
