ea6dca9894d1f0bfda6bea491bff4ff74235f0e0
Mammoth55 <slamam@mail.ru>
2020-04-14 01:31:32 +0600

    6. В классе StartUI заменить вывод в консоль на Consumer.[#233882]

--- chapter_002/src/main/java/ru/job4j/lambda/FuncInDiapason.java
+++ chapter_002/src/main/java/ru/job4j/lambda/FuncInDiapason.java
@@ -4,18 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Function;
 
-public class FuncInDiapason implements Function {
-
-    /**
-     * Applies this function to the given argument.
-     *
-     * @param o the function argument
-     * @return the function result
-     */
-    @Override
-    public Object apply(Object o) {
-        return o;
-    }
+public class FuncInDiapason {
 
     List<Double> diapason(int start, int end, Function<Double, Double> func) {
         List<Double> result = new ArrayList();
--- chapter_002/src/main/java/ru/job4j/search/PhoneDictionary.java
+++ chapter_002/src/main/java/ru/job4j/search/PhoneDictionary.java
@@ -1,10 +1,13 @@
 package ru.job4j.search;
 
+import ru.job4j.lambda.FuncInDiapason;
 import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
 
 public class PhoneDictionary {
 
-    private ArrayList<Person> persons = new ArrayList<Person>();
+    private List<Person> persons = new ArrayList<Person>();
 
     public void add(Person person) {
         this.persons.add(person);
@@ -16,11 +19,15 @@ public class PhoneDictionary {
      * @return Список найденных пользователей.
      */
     public ArrayList<Person> find(String key) {
+        Predicate<Person> personName = (Person) -> Person.getName().contains(key);
+        Predicate<Person> personSurName = (Person) -> Person.getSurname().contains(key);
+        Predicate<Person> personPhone = (Person) -> Person.getPhone().contains(key);
+        Predicate<Person> personAddress = (Person) -> Person.getAddress().contains(key);
+        Predicate<Person> combine = personName.or(personSurName).or(personPhone).or(personAddress);
         ArrayList<Person> result = new ArrayList<>();
-        for (Person p : persons) {
-            if (p.getName().contains(key) || p.getSurname().contains(key)
-                    || p.getPhone().contains(key) || p.getAddress().contains(key)) {
-                result.add(p);
+        for (Person person : persons) {
+            if (combine.test(person)) {
+                result.add(person);
             }
         }
         return result;
