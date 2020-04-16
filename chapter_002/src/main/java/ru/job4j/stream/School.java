bf4fca7845ac1e71b1909487828688b6519e954a
Mammoth55 <slamam@mail.ru>
2020-04-16 18:42:58 +0600

    1. Фильтрация учеников.[#233890] fixed 2.0

--- chapter_002/src/main/java/ru/job4j/stream/School.java
+++ chapter_002/src/main/java/ru/job4j/stream/School.java
@@ -6,12 +6,6 @@ import java.util.stream.Collectors;
 
 public class School {
 
-    public List<Student> students = List.of(
-            new Student("Ivanov", 55),
-            new Student("Mamontov", 40),
-            new Student("Arsentev", 100)
-    );
-
     public List<Student> collecting(List<Student> students, Predicate<Student> predict) {
         return students.stream().filter(predict).collect(Collectors.toList());
     }
--- chapter_002/src/test/java/ru/job4j/stream/SchoolTest.java
+++ chapter_002/src/test/java/ru/job4j/stream/SchoolTest.java
@@ -1,5 +1,6 @@
 package ru.job4j.stream;
 
+import org.junit.Before;
 import org.junit.Test;
 import java.util.List;
 import static org.hamcrest.Matchers.is;
@@ -7,10 +8,21 @@ import static org.junit.Assert.assertThat;
 
 public class SchoolTest {
 
+    List<Student> students;
+
+    @Before
+    public void dataInit() {
+        students = List.of(
+                new Student("Ivanov", 55),
+                new Student("Mamontov", 40),
+                new Student("Arsentev", 100)
+        );
+    }
+
     @Test
     public void whenClassA() {
         School school = new School();
-        List<Student> list = school.collecting(school.students, student ->
+        List<Student> list = school.collecting(students, student ->
                 student.getScore() <= 100 && student.getScore() >= 70);
         assertThat(list.get(0).getName(), is("Arsentev"));
     }
@@ -18,7 +30,7 @@ public class SchoolTest {
     @Test
     public void whenClassB() {
         School school = new School();
-        List<Student> list = school.collecting(school.students, student ->
+        List<Student> list = school.collecting(students, student ->
                 student.getScore() < 70 && student.getScore() >= 50);
         assertThat(list.get(0).getName(), is("Ivanov"));
     }
@@ -26,7 +38,7 @@ public class SchoolTest {
     @Test
     public void whenClassC() {
         School school = new School();
-        List<Student> list = school.collecting(school.students, student ->
+        List<Student> list = school.collecting(students, student ->
                 student.getScore() < 50 && student.getScore() >= 0);
         assertThat(list.get(0).getName(), is("Mamontov"));
     }
