9210f91d43471953d358f9bf53948e11f8d38d1c
Mammoth55 <slamam@mail.ru>
2020-05-09 20:26:01 +0600

    1. Collection API Улучшения[#233897]

--- chapter_002/src/test/java/ru/job4j/collection/NotifyAccountTest.java
+++ chapter_002/src/test/java/ru/job4j/collection/NotifyAccountTest.java
@@ -4,6 +4,8 @@ import org.junit.Test;
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
 
@@ -11,32 +13,24 @@ public class NotifyAccountTest {
 
     @Test
     public void sentAll() {
-        List<Account> accounts = Arrays.asList(
+        List<Account> accounts = List.of(
                 new Account("123", "Petr Arsentev", "eDer3432f"),
-                new Account("142", "Petr Arsentev", "000001")
-        );
-        HashSet<Account> expect = new HashSet<>(
-                Arrays.asList(
-                        new Account("123", "Petr Arsentev", "eDer3432f"),
-                        new Account("142", "Petr Arsentev", "000001")
-                )
-        );
+                new Account("142", "Petr Arsentev", "000001"));
+        Set<Account> expect = Set.of(
+                new Account("123", "Petr Arsentev", "eDer3432f"),
+                new Account("142", "Petr Arsentev", "000001"));
         assertThat(NotifyAccount.sent(accounts), is(expect));
     }
 
     @Test
     public void sentUnique() {
-        List<Account> accounts = Arrays.asList(
+        List<Account> accounts = List.of(
                 new Account("123", "Petr Arsentev", "eDer3432f"),
                 new Account("142", "Petr Arsentev", "000001"),
-                new Account("123", "Petr Arsentev", "eDer3432f")
-        );
-        HashSet<Account> expect = new HashSet<>(
-                Arrays.asList(
-                        new Account("123", "Petr Arsentev", "eDer3432f"),
-                        new Account("142", "Petr Arsentev", "000001")
-                )
-        );
+                new Account("123", "Petr Arsentev", "eDer3432f"));
+        Set<Account> expect = Set.of(
+                new Account("123", "Petr Arsentev", "eDer3432f"),
+                new Account("142", "Petr Arsentev", "000001"));
         assertThat(NotifyAccount.sent(accounts), is(expect));
     }
 }
--- chapter_002/src/test/java/ru/job4j/collection/UserTest.java
+++ chapter_002/src/test/java/ru/job4j/collection/UserTest.java
@@ -3,7 +3,6 @@ package ru.job4j.collection;
 import org.junit.Test;
 import java.util.Iterator;
 import java.util.Set;
-import java.util.TreeSet;
 import static org.hamcrest.Matchers.greaterThan;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
@@ -12,9 +11,9 @@ public class UserTest {
 
     @Test
     public void whenAsc() {
-        Set<User> users = new TreeSet<>();
-        users.add(new User("Petr", 32));
-        users.add(new User("Ivan", 31));
+        Set<User> users = Set.of(
+            new User("Petr", 32),
+            new User("Ivan", 31));
         Iterator<User> it = users.iterator();
         assertThat(it.next(), is(new User("Ivan", 31)));
         assertThat(it.next(), is(new User("Petr", 32)));
@@ -22,10 +21,10 @@ public class UserTest {
 
     @Test
     public void whenNameEquals() {
-        Set<User> users = new TreeSet<>();
-        users.add(new User("Petr", 32));
-        users.add(new User("Ivan", 31));
-        users.add(new User("Petr", 20));
+        Set<User> users = Set.of(
+                new User("Ivan", 31),
+                new User("Petr", 20),
+                new User("Petr", 32));
         Iterator<User> it = users.iterator();
         assertThat(it.next(), is(new User("Ivan", 31)));
         assertThat(it.next(), is(new User("Petr", 20)));
